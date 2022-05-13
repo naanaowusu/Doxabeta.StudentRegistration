5d2072ed34b370df8139895ee7b55a09408d8e57
Flexberry-man <mail@flexberry.net>
2022-05-13 10:41:24 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,18 +22,19 @@ export default Controller.extend({
             link: 'i-i-s-student-registration-student-l',
             caption: i18n.t('forms.application.sitemap.student-registration.i-i-s-student-registration-student-l.caption'),
             title: i18n.t('forms.application.sitemap.student-registration.i-i-s-student-registration-student-l.title'),
-            icon: 'paperclip',
+            icon: 'suitcase',
             children: null
           }, {
             link: 'i-i-s-student-registration-registration-l',
             caption: i18n.t('forms.application.sitemap.student-registration.i-i-s-student-registration-registration-l.caption'),
             title: i18n.t('forms.application.sitemap.student-registration.i-i-s-student-registration-registration-l.title'),
-            icon: 'paperclip',
+            icon: 'calendar',
             children: null
           }, {
             link: 'i-i-s-student-registration-course-l',
             caption: i18n.t('forms.application.sitemap.student-registration.i-i-s-student-registration-course-l.caption'),
             title: i18n.t('forms.application.sitemap.student-registration.i-i-s-student-registration-course-l.title'),
+            icon: 'folder open',
             children: null
           }]
         }
